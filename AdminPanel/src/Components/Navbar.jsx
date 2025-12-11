70d97e55865e0e44f32053941540c7acaf7a5d4f
syedkhizar123 <khizarrehman2007@gmail.com>
2025-12-11 14:49:31 +0500

    Update Navbar.jsx

--- AdminPanel/src/Components/Navbar.jsx
+++ AdminPanel/src/Components/Navbar.jsx
@@ -1,4 +1,4 @@
-import { Nav } from "react-bootstrap"
+import {Nav} from "react-bootstrap"
 import { NavLink } from "react-router-dom"
 
 export const Navbar = () => {
@@ -10,16 +10,17 @@ export const Navbar = () => {
                 <div className="my-[20px]  ">
                     <p className="text-2xl text-black-800 font-semibold pt-1"> EASYWEAR </p>
                 </div>
-                <div className="my-[20px]">''                    <Nav.Link as={NavLink} to="https://e-commerce-frontend-seven-beta.vercel.app/" >
-                    <button className="px-4 py-2  bg-gray-600 text-white font-medium rounded hover:bg-gray-700">
+                <div className="my-[20px]">
+                    <Nav.Link as={NavLink} to="https://e-commerce-adminpanel-iota.vercel.app/" >
+                        <button className="px-4 py-2  bg-gray-600 text-white font-medium rounded hover:bg-gray-700">
                         Logout
                     </button>
-                </Nav.Link>
-
+                    </Nav.Link>
+                    
                 </div>
             </div>
             <div className="w-[100%] border-b border-gray-300 " >
-
+               
             </div>
         </>
     )
